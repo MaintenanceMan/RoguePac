10b003d46722e6f124032c782543688abe06449f
Maintenance Man <softwaremaintenance@gmail.com>
2015-07-25 00:30:09 -0400

    2015 7DRL Entry
    Built with Visual Studio 2010 Professional

---
+++ BackupOneGhost/Form1.vb
@@ -0,0 +1,307 @@
+﻿Public Class Form1
+    Private Const gEMPTY = " "
+    Private Const gWALL = "#"
+    Private Const gPLAYER = "@"
+    Private Const gGHOST = "G"
+
+    Private Const gSTART_HIT_POINTS = 10
+    Private Const gMAX_HIT_POINTS = gSTART_HIT_POINTS
+    Private Const gREGNERATE_HP = 1
+    Private Const gPLAYER_DAMAGE = 5
+    Private Const gGHOST_DAMAGE = 4
+
+    Private Const gMAX_ROWS = 24
+    Private Const gMAX_COLS = 60
+    Dim gArrMaze(gMAX_ROWS - 1, gMAX_COLS - 1) As Char
+
+    Dim gPlayerX As Integer
+    Dim gPlayerY As Integer
+    Dim gPlayerHP As Integer
+
+    Dim gGhostX As Integer
+    Dim gGhostY As Integer
+    Dim gGhostHP As Integer
+
+    Dim gStrMessage As String
+
+    Dim gPlaying As Boolean
+
+    Private Sub InitMessage()
+        gStrMessage = ""
+    End Sub
+
+    Private Sub AddMessage(ByRef strMsg As String)
+        If (gStrMessage.Length = 0) Then
+            gStrMessage = strMsg
+        Else
+            gStrMessage = gStrMessage + " " + strMsg
+        End If
+    End Sub
+
+    Private Sub InitMaze()
+        ' Blank out the whole maze
+        For nRow = 0 To gMAX_ROWS - 1
+            For nCol = 0 To gMAX_COLS - 1
+                gArrMaze(nRow, nCol) = gEMPTY
+            Next
+        Next
+        ' Create a top and bottom walls
+        For nCol = 0 To gMAX_COLS - 1
+            gArrMaze(0, nCol) = gWALL
+            gArrMaze(gMAX_ROWS - 1, nCol) = gWALL
+        Next
+        ' Create a left and right walls
+        For nRow = 0 To gMAX_ROWS - 1
+            gArrMaze(nRow, 0) = gWALL
+            gArrMaze(nRow, gMAX_COLS - 1) = gWALL
+        Next
+        ' Add random walls
+        Randomize()
+        For nWall = 0 To gMAX_ROWS * gMAX_COLS / 4
+            Dim nX = CInt(Int(gMAX_COLS * Rnd()))
+            Dim nY = CInt(Int(gMAX_ROWS * Rnd()))
+            gArrMaze(nY, nX) = gWALL
+        Next
+    End Sub
+
+    Private Sub InitPlayer()
+        Dim nX As Integer
+        Dim nY As Integer
+        Dim bPlaced As Boolean
+        bPlaced = False
+        Do Until bPlaced
+            ' Propose a random location to start player
+            nX = CInt(Int(gMAX_COLS * Rnd()))
+            nY = CInt(Int(gMAX_ROWS * Rnd()))
+            ' Check if that location is occupied
+            bPlaced = (gArrMaze(nY, nX) = gEMPTY)
+        Loop
+        gPlayerX = nX
+        gPlayerY = nY
+        gArrMaze(gPlayerY, gPlayerX) = gPLAYER
+        gPlayerHP = gSTART_HIT_POINTS
+        gPlaying = True
+    End Sub
+
+    Private Sub InitGhost()
+        Dim nX As Integer
+        Dim nY As Integer
+        Dim bPlaced As Boolean
+        bPlaced = False
+        Do Until bPlaced
+            ' Propose a random location to start ghost
+            nX = CInt(Int(gMAX_COLS * Rnd()))
+            nY = CInt(Int(gMAX_ROWS * Rnd()))
+            ' Check if that location is occupied
+            bPlaced = (gArrMaze(nY, nX) = gEMPTY)
+        Loop
+        gGhostX = nX
+        gGhostY = nY
+        gArrMaze(gGhostY, gGhostX) = gGHOST
+        gGhostHP = gSTART_HIT_POINTS
+    End Sub
+
+    Private Sub ErasePlayer()
+        gArrMaze(gPlayerY, gPlayerX) = gEMPTY
+    End Sub
+
+    Private Sub EraseGhost()
+        gArrMaze(gGhostY, gGhostX) = gEMPTY
+    End Sub
+
+    Private Function CanMove(ByVal nX, ByVal nY) As Boolean
+        Dim bCanMove As Boolean
+        bCanMove = False
+        If (gArrMaze(nY, nX) = gEMPTY) Then
+            bCanMove = True
+        End If
+        Return bCanMove
+    End Function
+
+    Private Function IsGhost(ByVal nX, ByVal nY) As Boolean
+        Dim bIsGhost As Boolean
+        bIsGhost = False
+        If (gArrMaze(nY, nX) = gGHOST) Then
+            bIsGhost = True
+        End If
+        Return bIsGhost
+    End Function
+
+    Private Function IsPlayer(ByVal nX, ByVal nY) As Boolean
+        Dim bIsPlayer As Boolean
+        bIsPlayer = False
+        If (gArrMaze(nY, nX) = gPLAYER) Then
+            bIsPlayer = True
+        End If
+        Return bIsPlayer
+    End Function
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        InitMaze()
+        InitPlayer()
+        InitGhost()
+    End Sub
+
+    Private Sub Form1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
+        e.Graphics.Clear(Color.Black)
+        Dim fontObj As Font
+        fontObj = New System.Drawing.Font("Courier New", 14, FontStyle.Regular)
+        ' Show the maze
+        For nRow = 0 To gMAX_ROWS - 1
+            Dim strRow As String
+            strRow = ""
+            For nCol = 0 To gMAX_COLS - 1
+                strRow += gArrMaze(nRow, nCol)
+            Next
+            e.Graphics.DrawString(strRow, fontObj, Brushes.White, 0, nRow * 16)
+        Next
+        ' Show stats
+        Dim strStats As String
+        strStats = "HP: " + CStr(gPlayerHP)
+        e.Graphics.DrawString(strStats, fontObj, Brushes.White, 0, gMAX_ROWS * 16)
+        ' Show the latest messagesv
+        e.Graphics.DrawString(gStrMessage, fontObj, Brushes.White, 0, (gMAX_ROWS + 1) * 16)
+    End Sub
+
+    Private Sub MoveGhost(ByVal nX As Integer, ByVal nY As Integer)
+        EraseGhost()
+        gGhostX = nX
+        gGhostY = nY
+        gArrMaze(gGhostY, gGhostX) = gGHOST
+    End Sub
+
+    Private Sub HitPlayer()
+        gPlayerHP = gPlayerHP - gGHOST_DAMAGE
+        AddMessage("The ghost hits you.")
+        If (gPlayerHP <= 0) Then
+            gPlaying = False
+            AddMessage("You die.")
+        End If
+    End Sub
+
+    Private Sub ProcessGhost()
+        Dim nNewX As Integer
+        Dim nNewY As Integer
+        nNewX = gGhostX
+        nNewY = gGhostY
+        ' Determine optimal move toward player
+        If (gPlayerX > gGhostX) Then
+            nNewX = nNewX + 1
+        ElseIf (gPlayerX < gGhostX) Then
+            nNewX = nNewX - 1
+        End If
+        If (gPlayerY > gGhostY) Then
+            nNewY = nNewY + 1
+        ElseIf (gPlayerY < gGhostY) Then
+            nNewY = nNewY - 1
+        End If
+        If CanMove(nNewX, nNewY) Then
+            MoveGhost(nNewX, nNewY)
+        ElseIf IsPlayer(nNewX, nNewY) Then
+            HitPlayer()
+        Else
+            ' Determine just horizontal move toward player
+            nNewX = gGhostX
+            nNewY = gGhostY
+            If (gPlayerX > gGhostX) Then
+                nNewX = nNewX + 1
+            ElseIf (gPlayerX < gGhostX) Then
+                nNewX = nNewX - 1
+            End If
+            If CanMove(nNewX, nNewY) Then
+                MoveGhost(nNewX, nNewY)
+            ElseIf IsPlayer(nNewX, nNewY) Then
+                HitPlayer()
+            Else
+                ' Determine just vertical move toward player
+                nNewX = gGhostX
+                nNewY = gGhostY
+                If (gPlayerY > gGhostY) Then
+                    nNewY = nNewY + 1
+                ElseIf (gPlayerY < gGhostY) Then
+                    nNewY = nNewY - 1
+                End If
+                If CanMove(nNewX, nNewY) Then
+                    MoveGhost(nNewX, nNewY)
+                ElseIf IsPlayer(nNewX, nNewY) Then
+                    HitPlayer()
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub Regenerate()
+        gPlayerHP = gPlayerHP + gREGNERATE_HP
+        If (gPlayerHP > gMAX_HIT_POINTS) Then
+            gPlayerHP = gMAX_HIT_POINTS
+        End If
+    End Sub
+
+    Private Sub ProcessPlayer(ByVal nX As Integer, ByVal nY As Integer)
+        Dim bStep As Boolean
+        bStep = False
+        If CanMove(nX, nY) Then
+            ErasePlayer()
+            gPlayerX = nX
+            gPlayerY = nY
+            gArrMaze(gPlayerY, gPlayerX) = gPLAYER
+            bStep = True
+        ElseIf IsGhost(nX, nY) Then
+            gGhostHP = gGhostHP - gPLAYER_DAMAGE
+            AddMessage("You hit the ghost.")
+            If (gGhostHP <= 0) Then
+                EraseGhost()
+                InitGhost()
+                AddMessage("You kill the ghost.")
+            End If
+            bStep = True
+        End If
+        If bStep Then
+            Regenerate()
+            ProcessGhost()
+            Me.Refresh()
+            InitMessage()
+        End If
+    End Sub
+
+    Private Sub Form1_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles MyBase.KeyPress
+        Dim bAction As Boolean
+        bAction = False
+        Dim nNewX As Integer
+        Dim nNewY As Integer
+        nNewX = gPlayerX
+        nNewY = gPlayerY
+        If (e.KeyChar = "8") Then
+            nNewY = nNewY - 1
+            bAction = True
+        ElseIf (e.KeyChar = "9") Then
+            nNewY = nNewY - 1
+            nNewX = nNewX + 1
+            bAction = True
+        ElseIf (e.KeyChar = "6") Then
+            nNewX = nNewX + 1
+            bAction = True
+        ElseIf (e.KeyChar = "3") Then
+            nNewY = nNewY + 1
+            nNewX = nNewX + 1
+            bAction = True
+        ElseIf (e.KeyChar = "2") Then
+            nNewY = nNewY + 1
+            bAction = True
+        ElseIf (e.KeyChar = "1") Then
+            nNewY = nNewY + 1
+            nNewX = nNewX - 1
+            bAction = True
+        ElseIf (e.KeyChar = "4") Then
+            nNewX = nNewX - 1
+            bAction = True
+        ElseIf (e.KeyChar = "7") Then
+            nNewY = nNewY - 1
+            nNewX = nNewX - 1
+            bAction = True
+        End If
+        If gPlaying And bAction Then
+            ProcessPlayer(nNewX, nNewY)
+        End If
+    End Sub
+End Class
---
+++ History.vb
@@ -0,0 +1,9 @@
+﻿Module History
+    ' Version 1.0, 03/09/2015
+    '   - initial release for 2015 7DRL
+    ' Version 1.1
+    '   - added images for maze symbology
+    Public Function GetVersion() As String
+        Return "1.1"
+    End Function
+End Module
